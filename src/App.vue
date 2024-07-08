cfc370bfb5555e716e3963fb120c4d44b05cd9bd
zhy667 <hongyu.zhang@empatiohq.com>
2024-07-08 15:46:04 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app">
+      <BlogPost title="My First Blog Post" content="This is the content of my first blog post." />
+      <BlogPost title="My Second Blog Post" content="This is the content of my second blog post." />
+    </div>
+  </template>
+  
+  <script>
+  import BlogPost from './components/BlogPost.vue'
+  
+  export default {
+    name: 'App',
+    components: {
+      BlogPost
+    }
+  }
+  </script>
+  
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  </style>
+  
---
+++ src/components/BlogPost.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="blog-post">
+      <h1>{{ title }}</h1>
+      <p>{{ content }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'BlogPost',
+    props: {
+      title: String,
+      content: String
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .blog-post {
+    margin: 20px;
+  }
+  </style>
+  
